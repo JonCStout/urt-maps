8ea04eb7e5d5ea1c5da4ae8f0f255b612785e4d4
Jon Stout <jon-gh@abovecs.com>
2020-09-26 15:42:05 -0600

    change to use hooks for db;  minor cosmetics

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css'; // style sheet for just the app component
 import { Stitch, AnonymousCredential } from 'mongodb-stitch-browser-sdk'; // package for connecting to our MongoDB-Realm app; Realm is the new name for Stitch but this package still works
 
-// function taken from mongodb-stitch-browser-sdk quick start, with our app id:
-function initializeAndLogin() {
-    const client = Stitch.initializeDefaultAppClient('fsk-realmapp-slofx');
-    client.auth.loginWithCredential(new AnonymousCredential()).then((user) => {
-        document.getElementById(
-            'auth-status'
-        ).innerHTML = `Logged in as anonymous user with id ${user.id}`;
-    });
-}
-
-window.onload = initializeAndLogin; // call our function above once the web page has finished its first full render
-
 function App() {
+    const [isConnected, setIsConnected] = useState('is NOT connected');
+    const mongoClient = useRef(); // for saving the mongoClient object across renders of this component
+
+    useEffect(() => {
+        setIsConnected('is connecting...');
+        // initializeDefaultAppClient is really picky, only wants to be run once.  And saving the referenece to it is also picky
+        mongoClient.current = Stitch.initializeDefaultAppClient('fsk-realmapp-slofx'); // string is app ID (realmApp)
+
+        const creds = new AnonymousCredential();
+        mongoClient.current.auth.loginWithCredential(creds).then(() => {
+            setIsConnected('IS connected');
+            getAllRecords();
+        });
+    }, []); // the empty array at the end means this hook only runs once, after the web page is done with the initial render
+
+    function getAllRecords() {
+        if (mongoClient.current.auth.isLoggedIn) console.log('connected to db...');
+        console.log(mongoClient);
+    }
+
+    // return is what renders the html of our component:
     return (
         <div className="App">
             <header className="App-header">
-                <h1>
+                {/* <h1>
                     <div id="auth-status">Not logged in yet...</div>
-                </h1>
+                </h1> */}
+                <h2>database {isConnected}</h2>
             </header>
         </div>
     );
