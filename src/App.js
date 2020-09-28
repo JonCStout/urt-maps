e4bea0ffbc2c3a170afb33682b955eae02f4da9d
Jon Stout <jon-gh@abovecs.com>
2020-09-27 18:14:51 -0600

    added search box;  updated some screenshots

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect, useRef } from 'react';
-import './App.css'; // style sheet for just this app component
 import { Stitch, AnonymousCredential } from 'mongodb-stitch-browser-sdk'; // package for connecting to our MongoDB-Realm app; Realm is the new name for Stitch but this package still works
+import { Form, Input } from 'reactstrap'; // library for React controls
 import MapCard from './MapCard';
+import './App.css'; // style sheet for just this app component
 
 export default function App() {
     const [isConnected, setIsConnected] = useState('is NOT connected');
     const mongoClient = useRef(); // for saving the mongoClient object across renders of this component;  *** may not need to save this if only used in one function
     // const mongoUser = useRef();  // *** not sure we need to save this
     const [maps, setMaps] = useState();
+    const [searchInput, setSearchInput] = useState();
 
     useEffect(() => {
         setIsConnected('is connecting...');
@@ -16,6 +18,7 @@ export default function App() {
 
         const creds = new AnonymousCredential();
         mongoClient.current.auth.loginWithCredential(creds).then((user) => {
+            // *** user unneeded? ^
             setIsConnected('IS connected');
             // mongoUser.current = user;
             mongoClient.current.callFunction('getAllMapData').then((response) => {
@@ -26,15 +29,10 @@ export default function App() {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []); // the empty array at the end means this hook only runs once, after the web page is done with the initial render
 
-    // function getAllRecords() {
-    //     if (mongoClient.current.auth.isLoggedIn) console.log('connected to db...');
-    //     // console.log(mongoClient);
-    //     // console.log(mongoUser);
-    //     mongoClient.current.callFunction('getAllMapData').then((response) => {
-    //         setMaps(response.result);
-    //         console.log(response.result);
-    //     });
-    // }
+    function handleSearchChange(event) {
+        setSearchInput(event.target.value);
+        // todo:  parse input in real-time and generate tags?
+    }
 
     // return is what renders the html of our component:
     return (
@@ -42,7 +40,22 @@ export default function App() {
             <header className="App-header">
                 <h1>URT MAPS - (database {isConnected})</h1>
             </header>
-            <div id="searchBar">(Search bar here)</div>
+            <Form
+                id="searchBar"
+                onSubmit={(e) => {
+                    e.preventDefault();
+                }}
+            >
+                <Input
+                    type="search"
+                    name="search"
+                    id="searchBox"
+                    value={searchInput}
+                    onChange={handleSearchChange}
+                    placeholder="start typing map keywords here"
+                />
+            </Form>
+            {/* <div>Typed text: {searchInput}</div> */}
             <h2>Popular tags</h2>
             <div>list of tags here...</div>
             <br />
