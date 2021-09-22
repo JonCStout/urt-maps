73ca83be35cd3956382fc29f5a5167ec250e4fa8
JonCStout <jon-gh@abovecs.com>
2021-09-22 11:57:50 -0600

    nav arrows, centering, sizing

--- src/MapDetailPage.js
+++ src/MapDetailPage.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Box from '@material-ui/core/Box';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation } from 'swiper';
 import 'swiper/swiper.min.css';
+import 'swiper/components/navigation/navigation.min.css';
 import './MapDetailPage.css';
 
+SwiperCore.use([Navigation]);
 function MapDetailPage({ map, ssIdx }) {
     const prefix = `ss/${map._id}/`; // path prefix
     const ss = map.screenShots; // coding shortcut
 
     return (
         <>
-            <Box height='75%' width='80%'>
-                <Swiper className='mySwiper'>
-                    {ss.map((_el, index) => {
-                        return (
-                            <SwiperSlide>
-                                <img className='detail-main-ss' src={prefix + _el} alt={'screenshot ' + (index + 1)} />
-                            </SwiperSlide>
-                            // <img style={{ height: '600px' }} src={prefix + _el} alt={'screenshot ' + (index + 1)} />
-                        );
-                    })}
-                </Swiper>
-            </Box>
+            <Swiper navigation={true} spaceBetween={20} centeredSlides={true} className='mySwiper'>
+                {ss.map((_el, index) => {
+                    return (
+                        <SwiperSlide>
+                            <img src={prefix + _el} alt={'screenshot ' + (index + 1)} />
+                        </SwiperSlide>
+                    );
+                })}
+            </Swiper>
         </>
     );
 }
