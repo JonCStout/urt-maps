2d468d5f49a2abbe3d749f10d81475c711e437bb
JonCStout <jon-gh@abovecs.com>
2022-01-23 16:02:50 -0700

    routing tweaks, doc title (collaborated)

--- src/MapCard.js
+++ src/MapCard.js
@@ -12,35 +12,32 @@ export default function MapCard({ map, cb }) {
     return (
         <>
             <div className='card'>
-                <Link to={'/map/' + displayName}>
-                    <button className='bare-button-main'>
-                        <img className='main-ss' src={prefix + ss[0]} alt='main screenshot' />
-                    </button>
-                    <span>
-                        <button className='bare-button-sub bare-button-sub-left'>
-                            <img className='sub-ss' src={prefix + ss[1]} alt='screenshot 2' />
-                        </button>
-                        <button className='bare-button-sub bare-button-sub-right'>
-                            <img className='sub-ss' src={prefix + ss[2]} alt='screenshot 3' />
-                        </button>
-                    </span>
-                    <Grid container direction='row' justify='center' alignItems='center'>
-                        <Grid item xs={2}>
-                            &nbsp;
-                        </Grid>
-                        <Grid item xs={8}>
-                            <h2 id='title'>{displayName}</h2>
-                        </Grid>
-                        <Grid item xs={2}>
-                            {ss.length > 3 ? (
-                                <button className='more-button' title='more screenshots'>
-                                    {/* ^ *** link to carousel later */}
-                                    +more screenshots
-                                </button>
-                            ) : null}
-                        </Grid>
-                    </Grid>
+                <Link to={'/map/' + displayName + '?ss=0'} className='bare-button-main'>
+                    <img className='main-ss' src={prefix + ss[0]} alt='main screenshot' />
                 </Link>
+                <span>
+                    <Link to={'/map/' + displayName + '?ss=1'} className='bare-button-sub bare-button-sub-left'>
+                        <img className='sub-ss' src={prefix + ss[1]} alt='screenshot 2' />
+                    </Link>
+                    <Link to={'/map/' + displayName + '?ss=2'} className='bare-button-sub bare-button-sub-right'>
+                        <img className='sub-ss' src={prefix + ss[2]} alt='screenshot 3' />
+                    </Link>
+                </span>
+                <Grid container direction='row' justify='center' alignItems='center'>
+                    <Grid item xs={2}>
+                        &nbsp;
+                    </Grid>
+                    <Grid item xs={8}>
+                        <h2 id='title'>{displayName}</h2>
+                    </Grid>
+                    <Grid item xs={2}>
+                        {ss.length > 3 ? (
+                            <Link to={'/map/' + displayName + '?ss=3'} className='more-button'>
+                                +more screenshots
+                            </Link>
+                        ) : null}
+                    </Grid>
+                </Grid>
             </div>
         </>
     );
