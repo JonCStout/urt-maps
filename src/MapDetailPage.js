48a9812160e96749d60688bf2f81419bcf4ef88d
Mandolin <urt.mandolin@gmail.com>
2022-01-29 17:51:17 -0600

    add thumbnails, make photo fullscreen

--- src/MapDetailPage.js
+++ src/MapDetailPage.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react';
 // import PropTypes from 'prop-types';
 import { Link, useParams, useSearchParams } from 'react-router-dom';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Navigation } from 'swiper';
+import SwiperCore, { Navigation, Thumbs } from 'swiper';
 import mapdb from './MapData2';
 import 'swiper/swiper.min.css'; // can't use swiper/css as in examples; need a specific .css file for now, (maybe React 18, webpack 5, or a fixed swiper@7 will change that)
 import 'swiper/components/navigation/navigation.min.css'; // ditto for swiper/css/navigation
 import './MapDetailPage.css';
 
-SwiperCore.use([Navigation]);
+SwiperCore.use([Navigation, Thumbs]);
 
 function MapDetailPage() {
     let params = useParams();
@@ -18,6 +18,7 @@ function MapDetailPage() {
     const [searchParams] = useSearchParams(); // don't need set function, so don't destructure it
     const [map, updateMap] = useState(null);
     const [queryState, updateQueryState] = useState(queryStates.NOTSTARTED);
+    const [thumbsSwiper, setThumbsSwiper] = useState(null);
 
     useEffect(() => {
         mapdb.connect().then(() => {
@@ -47,13 +48,16 @@ function MapDetailPage() {
             )}
             {map && (
                 <div>
-                    <h1>Map: {map.pk3}</h1>
-                    <Link to='/'>Back to home</Link>
+                    <header className='header'>
+                        <h1>Map: {map.pk3}</h1>
+                        <Link to='/'>Back to home</Link>
+                    </header>
                     <Swiper
                         navigation={true}
                         spaceBetween={20}
                         centeredSlides={true}
                         className='mySwiper'
+                        thumbs={{ swiper: thumbsSwiper }}
                         initialSlide={searchParams.get('ss') || 0}
                     >
                         {map.screenShots.map((_el, index) => {
@@ -64,6 +68,32 @@ function MapDetailPage() {
                             );
                         })}
                     </Swiper>
+                    <main className='main'>
+                        <div className='thumbnails'>
+                            <Swiper
+                                onSwiper={setThumbsSwiper}
+                                loop={true}
+                                spaceBetween={10}
+                                slidesPerView={4}
+                                freeMode={true}
+                                watchSlidesProgress={true}
+                                modules={[Navigation, Thumbs]}
+                                className='mySwiper'
+                            >
+                                {map.screenShots.map((_el, index) => {
+                                    return (
+                                        <SwiperSlide key={index}>
+                                            <img src={'/ss/' + map._id + '/' + _el} alt={'screenshot ' + (index + 1)} />
+                                        </SwiperSlide>
+                                    );
+                                })}
+                            </Swiper>
+                        </div>
+                        <div className='mapDetails'>
+                            {map.pk3}
+                            etc.
+                        </div>
+                    </main>
                 </div>
             )}
         </div>
